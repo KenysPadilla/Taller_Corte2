e31dcff966230864f1eecf68716264e2f28866a4
Kenys Padilla <kvpadilla@unicesar.edu.co>
2026-04-15 09:52:17 -0500

    feat: trabajado en clase

--- src/main/java/com/mycompany/restauranteelbuensabor/Utilidades.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/Utilidades.java
@@ -9,30 +9,49 @@ package com.mycompany.restauranteelbuensabor;
  * @author alfre
  */
 public class Utilidades {
-public static double calcular(double pr,double cn,double dc,double iv,double pp,int ni,boolean ap){
-double res=0;double tmp=0;double aux2=0;
+
+    public static double calcular(double pr, double cn, double dc, double iv, double pp, int ni, boolean ap) {
+        double res = 0;
+        double tmp = 0;
+        double aux2 = 0;
 // calcula el resultado
-res=pr*cn;
-if(dc>0){res=res-(res*dc);}
-tmp=res*iv;res=res+tmp;
-if(ap){res=res+(res*pp);}
+        res = pr * cn;
+        if (dc > 0) {
+            res = res - (res * dc);
+        }
+        tmp = res * iv;
+        res = res + tmp;
+        if (ap) {
+            res = res + (res * pp);
+        }
 // imprime restaurante
-System.out.println("RESTAURANTE EL BUEN SABOR - calculo aplicado");
-aux2=res;
-return aux2;}
-public static boolean validar(){
-int cont=0;int i=0;
-while(i<Datos.cant.length){
-if(Datos.cant[i]>0){cont=cont+1;}
-i++;}// fin while
+        System.out.println("RESTAURANTE EL BUEN SABOR - calculo aplicado");
+        aux2 = res;
+        return aux2;
+    }
+
+    public static boolean validar() {
+        int cont = 0;
+        int i = 0;
+        while (i < Datos.cantidades.length) {
+            if (Datos.cantidades[i] > 0) {
+                cont = cont + 1;
+            }
+            i++;
+        }// fin while
 // reinicia si no hay nada - efecto secundario no documentado
-if(cont==0){Datos.tot=0;Datos.tmp="";}
-return cont>0;}
-public static void reiniciar(){
+        if (cont == 0) {
+            Datos.total = 0;
+            Datos.mensajeAuxiliar = "";
+        }
+        return cont > 0;
+    }
+
+    public static void reiniciar() {
 // metodo antiguo de calculo - pendiente revisar
-// public static double calcOld(double precio, int cant){
+// public static double calcOld(double precio, int cantidades){
 // double resultado = 0;
-// resultado = precio * cant;
+// resultado = precio * cantidades;
 // resultado = resultado + (resultado * 0.19);
 // if(resultado > 50000){
 // resultado = resultado + (resultado * 0.10);}
@@ -40,11 +59,19 @@ public static void reiniciar(){
 // System.out.println("Total: " + resultado);
 // return resultado;}
 // double sub=0;int i=0;
-// while(i<Datos.nom.length){
-// sub=sub+Datos.p[i]*Datos.cant[i];i++;}
+// while(i<Datos.nombres.length){
+// sub=sub+Datos.precios[i]*Datos.cantidades[i];i++;}
 // if(sub>50000){ sub=sub+(sub*0.19); sub=sub+(sub*0.10); }
 // else{ sub=sub+(sub*0.19); }
-// Datos.tot=sub;
-int i=0;while(i<Datos.cant.length){Datos.cant[i]=0;i++;}
-Datos.tot=0;Datos.est=0;Datos.ms=0;Datos.tmp="";}    
+// Datos.total=sub;
+        int i = 0;
+        while (i < Datos.cantidades.length) {
+            Datos.cantidades[i] = 0;
+            i++;
+        }
+        Datos.total = 0;
+        Datos.estadoMesa = 0;
+        Datos.numeroMesaActual = 0;
+        Datos.mensajeAuxiliar = "";
+    }
 }
